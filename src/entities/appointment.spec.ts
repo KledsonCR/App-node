a91d5e43039a2afcdfaeb8b19b0b4d9c1cd58ea4
kledson costa <kledsoncrabelo@icloud.com>
2022-09-11 16:15:05 -0300

    ref: add import and date validation

--- src/entities/appointment.spec.ts
+++ src/entities/appointment.spec.ts
@@ -1,12 +1,10 @@
 import { expect, test } from 'vitest'
+import { getValidDate } from '../tests/utils/get-valid-date'
 import { Appointment } from './appointment'
 
 test('create an appointment', () => {
-  const startsAt = new Date()
-  const endsAt = new Date()
-
-  startsAt.setDate(startsAt.getDate() + 1)
-  endsAt.setDate(endsAt.getDate() + 2)
+  const startsAt = getValidDate('2022-08-10')
+  const endsAt = getValidDate('2022-08-11')
 
   const appointment = new Appointment({
     customer: 'John Doe',
@@ -19,11 +17,8 @@ test('create an appointment', () => {
 })
 
 test('cannot create an appointment with end date before start date', () => {
-  const startsAt = new Date()
-  const endsAt = new Date()
-
-  startsAt.setDate(startsAt.getDate() + 2)
-  endsAt.setDate(endsAt.getDate() + 1)
+  const startsAt = getValidDate('2022-08-11')
+  const endsAt = getValidDate('2022-08-10')
 
   expect(() => {
     return new Appointment({
--- src/use-cases/create-appointment.spec.ts
+++ src/use-cases/create-appointment.spec.ts
@@ -1,16 +1,14 @@
 import { describe, expect, it } from 'vitest'
 import { Appointment } from '../entities/appointment'
+import { getValidDate } from '../tests/utils/get-valid-date'
 import { CreateAppointment } from './create-appointment'
 
 describe('Create Appointment', () => {
   it('should be able to create an appointment', () => {
+    const startsAt = getValidDate('2022-08-10')
+    const endsAt = getValidDate('2022-08-11')
+
     const createAppointment = new CreateAppointment()
-
-    const startsAt = new Date()
-    const endsAt = new Date()
-
-    startsAt.setDate(startsAt.getDate() + 1)
-    endsAt.setDate(endsAt.getDate() + 2)
 
     expect(createAppointment.execute({
       customer: 'John Doe',
