80586b83d4eda4202bbb038135f8bb08b32b925b
kledson costa <kledsoncrabelo@icloud.com>
2022-09-11 17:31:47 -0300

    test: add new unit tests

--- src/use-cases/create-appointment.spec.ts
+++ src/use-cases/create-appointment.spec.ts
@@ -20,4 +20,44 @@ describe('Create Appointment', () => {
       endsAt
     })).resolves.toBeInstanceOf(Appointment)
   })
+
+  it('should not be able to create an appointment with overlapping dates', async () => {
+    const startsAt = getValidDate('2022-08-10')
+    const endsAt = getValidDate('2022-08-15')
+
+    const appointmentsRepository = new InMemoryAppointmentsRepository()
+    const createAppointment = new CreateAppointment(
+      appointmentsRepository
+    )
+
+    await createAppointment.execute({
+      customer: 'John Doe',
+      startsAt,
+      endsAt
+    })
+
+    expect(createAppointment.execute({
+      customer: 'John Doe',
+      startsAt: getValidDate('2022-08-14'),
+      endsAt: getValidDate('2022-08-18')
+    })).rejects.toBeInstanceOf(Error)
+
+    expect(createAppointment.execute({
+      customer: 'John Doe',
+      startsAt: getValidDate('2022-08-08'),
+      endsAt: getValidDate('2022-08-12')
+    })).rejects.toBeInstanceOf(Error)
+
+    expect(createAppointment.execute({
+      customer: 'John Doe',
+      startsAt: getValidDate('2022-08-08'),
+      endsAt: getValidDate('2022-08-17')
+    })).rejects.toBeInstanceOf(Error)
+
+    expect(createAppointment.execute({
+      customer: 'John Doe',
+      startsAt: getValidDate('2022-08-11'),
+      endsAt: getValidDate('2022-08-12')
+    })).rejects.toBeInstanceOf(Error)
+  })
 })
